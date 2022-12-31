fd16cf1083b2e673915fad590120459ef34d8de3
Jay Sivadas <jhsivadas@live.com>
2022-12-30 21:55:13 -0800

    workign dropdownnvabar

--- website/js/nav.js
+++ website/js/nav.js
@@ -1,27 +1,9 @@
-// Targets the mobile menu id for the html
-const menu = document.querySelector('#mobile-menu')
-
-// This one targets a class
-const menuLinks = document.querySelector('.navbar__menu')
-
-// Display Mobile Menu function
-const mobileMenu = () => {
-    // Active or not active
-    // Toggles the is-active class
-    menu.classList.toggle('is-active')
-    // Toggles the active class
-    menuLinks.classList.toggle('active')
-}
-
-menu.addEventListener('click', mobileMenu);
-
-const button = document.querySelector('.btn')
-const form   = document.querySelector('.form')
-
-button.addEventListener('click', function() {
-   form.classList.add('form--no')
-});
-
-
-
-
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
